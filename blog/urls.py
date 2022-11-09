fa9e79597da6d0d4926da95f341219b9fa317fb6
Jody M <jodyamm@hotmail.com>
2022-11-09 09:14:07 +0000

    Add like icons and functionality

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path('featured/', views.FeaturedView.as_view(), name='featured'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
 ]
