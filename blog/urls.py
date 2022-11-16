7a496f416f5d4df5ff649db9bd62616c0fbf118d
Jody M <jodyamm@hotmail.com>
2022-11-16 11:48:13 +0000

    Add view for single feature post html

--- blog/urls.py
+++ blog/urls.py
@@ -9,11 +9,13 @@ from .models import Profile
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path('featured/', views.FeaturedView.as_view(), name='featured'),
+    path('featured_post/', views.FeaturedPost, name='featured_post'),
+    # path('featured_post/', FeaturedPost, name='featured_post'),
     path('profile/', login_required(UserProfile.as_view()), name='profile'),
     path(
         'edit_profile/', login_required(
             UserSettings.as_view()), name='edit_profile'
-        ),
+    ),
     path('add_post/', AddPostView.as_view(), name='add_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
--- blog/views.py
+++ blog/views.py
@@ -127,21 +127,6 @@ class FeaturedView(generic.TemplateView):
     template_name = "feature.html"
 
 
-# class FeaturedPost(generic.TemplateView):
-
-#     def get(self, request, slug, *args, **kwargs):
-#         queryset = Post.objects.filter(status=1)
-#         post = get_object_or_404(queryset, slug=slug)
-#         # comments = post.comments.filter(
-#         #     approved=True).order_by("-created_on")
-#         # liked = False
-#         # if post.likes.filter(id=self.request.user.id).exists():
-#         #     liked = True
-
-#         return render(
-#                 request,
-#                 "featured_post.html",
-#                 {
-#                     "post": post,
-#                 },
-#             )
+def FeaturedPost(request):
+    template_name = "featured_post.html"
+    return render(request, 'featured_post.html')
