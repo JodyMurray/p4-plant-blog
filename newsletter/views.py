440a55f54f8d911119e099fcd0a13cda53ce4892
Jody M <jodyamm@hotmail.com>
2022-12-01 08:45:39 +0000

    Email sending to terminal with debug on but not emailing when false

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.contrib import messages
 from django.shortcuts import render
+from django.core.mail import send_mail
 from django.views.generic import FormView
 from newsletter.forms import JoinForm
 from .models import NewsLetter
-
 
 
 def newsletter_signup(request):
@@ -13,12 +14,18 @@ def newsletter_signup(request):
         instance = form.save(commit=False)
         if NewsLetter.objects.filter(email=instance.email).exists():
             messages.warning(
-                request, "Your email already exists in our database", 
+                request, "Your email already exists in our database",
                 "alert alert-warning alert-dismissible")
         else:
             instance.save()
             messages.success(request, "Your email has been submitted to the database",
-            "alert alert-success alert-dismissible")
+                             "alert alert-success alert-dismissible")
+            subject = "Thank you for joining our Newsletter"
+            from_email = settings.EMAIL_HOST_USER
+            to_email = [instance.email]
+            signup_message = """ Welcome to Jody's Plant Blog Newsletter """
+            send_mail(subject=subject, from_email=from_email,
+                      recipient_list=to_email, message=signup_message, fail_silently=False)
 
     context = {
         'form': form,
@@ -35,14 +42,20 @@ def newsletter_unsubscribe(request):
         if NewsLetter.objects.filter(email=instance.email).exists():
             NewsLetter.objects.filter(email=instance.email).delete()
             messages.success(request, "Your email has been removed",
-            "alert alert-success alert-dismissible")
+                             "alert alert-success alert-dismissible")
+            subject = "You have unsubscribed to our Newsletter"
+            from_email = settings.EMAIL_HOST_USER
+            to_email = [instance.email]
+            signup_message = """ Sorry to see you go, let us know if there is an issue with our service """
+            send_mail(
+                subject=subject, from_email=from_email,
+                recipient_list=to_email, message=signup_message, fail_silently=False)
         else:
             messages.warning(request, "Your email is not in the database",
-            "alert alert-warning alert-dismissible")
+                             "alert alert-warning alert-dismissible")
 
     context = {
         'form': form,
     }
     template = "newsletter/unsubscribe.html"
     return render(request, template, context)
-  
--- plantblog/settings.py
+++ plantblog/settings.py
@@ -63,6 +63,23 @@ MESSAGE_TAGS = {
     messages.ERROR: 'alert-danger',
 }
 
+# EMAIL
+if DEBUG:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_HOST_USER = os.environ.get("EMAIL")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    # EMAIL_HOST = "smtp.gmail.com"
+    # EMAIL_PORT = 587
+    # EMAIL_USE_TLS = True
+    # EMAIL_HOST_USER = 'murrayjody08@gmail.com'
+    # EMAIL_HOST_PASSWORD = 'nsbkqnpojrzmvrkp'
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
