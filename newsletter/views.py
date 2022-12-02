8c9d348e1fcc4ed3193d51ab0d5726706f1e70f7
Jody M <jodyamm@hotmail.com>
2022-12-02 08:24:49 +0000

    Add email html and connect to free test service

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,40 +1,65 @@
 from django.conf import settings
 from django.contrib import messages
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.views.generic import FormView
 from newsletter.forms import JoinForm
 from .models import NewsLetter
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-
+from django.template.loader import render_to_string
+# from sendgrid import SendGridAPIClient
+# from sendgrid.helpers.mail import Mail
 
 
 def newsletter_signup(request):
-    form = JoinForm(request.POST or None)
+    if request.method == 'POST':
+        form = JoinForm(request.POST)
 
-    if form.is_valid():
-        instance = form.save(commit=False)
-        if NewsLetter.objects.filter(email=instance.email).exists():
-            messages.warning(
-                request, "Your email already exists in our database",
-                "alert alert-warning alert-dismissible")
-        else:
-            instance.save()
-            messages.success(request, "Your email has been submitted to the database",
-                             "alert alert-success alert-dismissible")
-            subject = "Thank you for joining our Newsletter"
-            from_email = settings.EMAIL_HOST_USER
-            to_email = [instance.email]
-            signup_message = """ Welcome to Jody's Plant Blog Newsletter """
-            send_mail(subject=subject, from_email=from_email,
-                      recipient_list=to_email, message=signup_message, fail_silently=False)
+        if form.is_valid():
+            #     instance = form.save(commit=False)
+            # if NewsLetter.objects.filter(email=instance.email).exists():
+            email = form.cleaned_data['email']
+            # print(form.cleaned_data['email'])
+            # print("Email valid")
+        # to_email = [instance.email]
 
-    context = {
-        'form': form,
-    }
-    template = "newsletter/register.html"
-    return render(request, template, context)
+            html = render_to_string('newsletter/email.html', {
+                'email': email,
+            })
+
+        send_mail('subject', 'message', settings.EMAIL_HOST_USER,
+                  [email], html_message=html)
+
+    else:
+        form = JoinForm(request.POST)
+
+    return render(request, 'newsletter/register.html', {
+        'form': form
+    })
+
+    # form = JoinForm(request.POST or None)
+
+    # if form.is_valid():
+    #     instance = form.save(commit=False)
+    #     if NewsLetter.objects.filter(email=instance.email).exists():
+    #         messages.warning(
+    #             request, "Your email already exists in our database",
+    #             "alert alert-warning alert-dismissible")
+    #     else:
+    #         instance.save()
+    #         messages.success(request, "Your email has been submitted to the database",
+    #                          "alert alert-success alert-dismissible")
+    #         subject = "Thank you for joining our Newsletter"
+    #         from_email = settings.EMAIL_HOST_USER
+    #         to_email = [instance.email]
+    #         signup_message = """ Welcome to Jody's Plant Blog Newsletter """
+    #         send_mail(subject=subject, from_email=from_email,
+    #                   recipient_list=to_email, message=signup_message, fail_silently=False)
+
+    # context = {
+    #     'form': form,
+    # }
+    # template = "newsletter/register.html"
+    # return render(request, template, context)
 
 
 def newsletter_unsubscribe(request):
