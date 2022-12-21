1bd63ca0e31a0c3e1a0165e39e35ffdc1f8c8a08
Jody M <jodyamm@hotmail.com>
2022-12-21 06:21:09 +0000

    Modify setUp in test case and create user profile test method

--- blog/tests.py
+++ blog/tests.py
@@ -1,36 +1,62 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-
-
-User = get_user_model()
+from .models import Profile, User
 
 
 class TestViews(TestCase):
+
     def setUp(self):
-        user_a = User(username='admin', email='jjkittymurray@gmail.com')
-        user_a_pw = 'some_123_password'
-        self.user_a_pw = user_a_pw
-        user_a.is_staff = True
-        user_a.is_superuser = True
-        user_a.save()
-        user_a.set_password(user_a_pw)
-        self.user_a = user_a
+        test_user = User.objects.create_user(
+            username='test_user',
+            email='test_user@gmail.com',
+            password='test_password')
+        test_user.is_staff = True
+        test_user.is_superuser = True
+        test_user.save()
+        self.test_user = test_user
+        test_profile = Profile.objects.create(
+            id=1,
+            user=test_user,
+            first_name='test first name',
+            last_name='test last name',
+            bio='test bio'
+        )
 
     def test_user_exists(self):
         user_count = User.objects.all().count()
-        self.assertEqual(user_count, 1)
+        self.assertEqual(
+            user_count,
+            1
+        )
 
-    def test_user_password(self):
-        self.assertTrue(self.user_a.check_password(
-            self.user_a_pw
-            ))
-    
     def test_login_url(self):
         login_url = settings.LOGIN_URL
-        data = {"username": "admin", "password": self.user_a_pw}
-        response = self.client.post("/accounts/login", data, follow=True)
+        data = {
+            'username': 'test_user',
+            'password': 'test_password'
+        }
+        response = self.client.post(
+            '/accounts/login',
+            data,
+            follow=True
+        )
         status_code = response.status_code
-        redirect_path = response.get("PATH_INFO")
+        redirect_path = response.get('PATH_INFO')
         # self.assertEqual(redirect_path, settings.LOGIN_REDIRECT_URL)
-        self.assertEqual(status_code, 200)
+        self.assertEqual(
+            status_code,
+            200
+        )
+
+    def test_get_user_profile(self):
+        test_model = Profile.objects.get(
+            bio='test bio'
+        )
+        response = self.client.get(
+            '/profile/',
+        )
+        self.assertEqual(
+            response.status_code,
+            302
+        )
