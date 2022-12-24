775b3e35e493f0e52dbe5e00636652c5b95e0688
Jody M <jodyamm@hotmail.com>
2022-12-24 16:27:32 +0000

    Migration commit

--- blog/models.py
+++ blog/models.py
@@ -110,9 +110,7 @@ class Comment(models.Model):
         max_length=80,
     )
     email = models.EmailField()
-    body = models.TextField(
-        validators=[validate_text_length]
-    )
+    body = models.TextField()
     created_on = models.DateTimeField(
         auto_now_add=True
     )
